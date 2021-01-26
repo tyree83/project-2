9d50b431abd8e8db8adc0047089eb66a3f242faf
Tyree Christensen <tyree83@gmail.com>
2021-01-26 15:29:11 -0500

    Deploy to Heroku

--- config/database.js
+++ config/database.js
@@ -13,7 +13,3 @@ mongoose.connect(process.env.DATABASE_URL, {
 db.on('connected', function () {
     console.log(`Connected to MongoDB at ${db.host}:${db.port}`);  
 })
-
-db.on("error", function(error) {
-    console.log(`Encountered the following error: ${error.message}`);
-});
--- controllers/users.js
+++ controllers/users.js
@@ -49,7 +49,7 @@ function login(req, res) {
                 //    add the user to a new session
                 req.session.userId = foundUser._id;
                 //   redirect to a secure location 
-                res.redirect('/plants');
+                res.redirect('/plants/index');
             } else {
                 // if the password is wrong - redirect back to login
                 res.redirect('/users/signin');
